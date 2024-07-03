7cb5a84c800fc9228ece7872b60de535642028b5
AldieAchmad <aldieachmad23@gmail.com>
2024-07-03 19:27:59 +0700

    Add files via upload

---
+++ penjualan.sql
@@ -0,0 +1,156 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 03, 2024 at 02:26 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_barang`
+--
+
+CREATE TABLE `tbl_barang` (
+  `kd_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `tanggal_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_barang`
+--
+
+INSERT INTO `tbl_barang` (`kd_barang`, `nama_barang`, `jumlah_barang`, `harga_beli`, `harga_jual`, `tanggal_masuk`) VALUES
+('B0001', 'Kemeja Linen Broken White', 9, 50000, 100000, '2024-06-19'),
+('B0002', 'Kemeja Linen Hitam', 7, 50000, 100000, '2024-06-30'),
+('B0003', 'Kemeja Linen Cream', 10, 50000, 100000, '2024-06-30'),
+('B0004', 'Kemeja Linen Biru Dongker', 11, 50000, 100000, '2024-06-30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_hitung_jual`
+--
+
+CREATE TABLE `tbl_hitung_jual` (
+  `id_hitung` int(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_jual` int(11) NOT NULL,
+  `harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Triggers `tbl_hitung_jual`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_jual` AFTER INSERT ON `tbl_hitung_jual` FOR EACH ROW BEGIN
+	UPDATE tbl_barang SET jumlah_barang=jumlah_barang-new.jumlah_jual
+    WHERE kd_barang=new.kd_barang;
+    END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_telp` int(30) NOT NULL,
+  `agama` varchar(30) NOT NULL,
+  `alamat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`username`, `password`, `jenis_kelamin`, `email`, `no_telp`, `agama`, `alamat`) VALUES
+('aldieachmad', '123456', 'Laki-Laki', 'aldie12@gmail.com', 81234567, 'Islam', 'bandung');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_penjualan`
+--
+
+CREATE TABLE `tbl_penjualan` (
+  `nofaktur` varchar(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_jual` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembalian` int(11) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_penjualan`
+--
+
+INSERT INTO `tbl_penjualan` (`nofaktur`, `kd_barang`, `nama_barang`, `hsatuan`, `jumlah_jual`, `harga`, `bayar`, `kembalian`, `tanggal`) VALUES
+('MC00001', 'B0002', 'Kemeja Hitam', 100000, 1, 100000, 110000, 10000, '2024-06-30'),
+('MC00002', 'B0001', 'Kemeja BW', 100000, 1, 100000, 120000, 20000, '2024-06-30'),
+('MC00003', 'B0002', 'Kemeja Hitam', 100000, 1, 100000, 120000, 20000, '2024-06-30'),
+('MC00004', 'B0001', 'Kemeja BW', 100000, 1, 100000, 100000, 0, '2024-06-30'),
+('MC0005', 'B0001', 'Kemeja BW', 100000, 1, 100000, 100000, 0, '2024-06-30'),
+('MC0006', 'B0003', 'Kemeja Cream', 100000, 1, 100000, 100000, 0, '2024-06-30'),
+('MC0007', 'B0003', 'Kemeja Cream', 100000, 1, 100000, 100000, 0, '2024-06-30'),
+('MC0008', 'B0002', 'Kemeja Hitam', 100000, 1, 100000, 100000, 0, '2024-06-30'),
+('MC0009', 'B0003', 'Kemeja Cream', 100000, 1, 100000, 100000, 0, '2024-06-30'),
+('MC0010', 'B0001', 'Kemeja Linen Broken White', 100000, 1, 100000, 100000, 0, '2024-07-01'),
+('MC0011', 'B0001', 'Kemeja Linen Broken White', 100000, 2, 200000, 200000, 0, '2024-07-03');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_hitung_jual`
+--
+ALTER TABLE `tbl_hitung_jual`
+  ADD PRIMARY KEY (`id_hitung`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_hitung_jual`
+--
+ALTER TABLE `tbl_hitung_jual`
+  MODIFY `id_hitung` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
